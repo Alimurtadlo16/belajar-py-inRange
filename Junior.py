6079de76409dc94d2828cd9102ce4bab92a4de89
Alimurtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-07-10 21:58:45 +0700

    How to make for i in range

---
+++ Junior.py
@@ -0,0 +1,7 @@
+import turtle
+t = turtle.Turtle()
+t.speed(0)
+for i in range(200):
+    t.forward(i)
+    t.left(91)
+turtle.done()
---
+++ Senior.py
@@ -0,0 +1,8 @@
+from turtle import *
+
+speed(0)
+
+for i in range(200):
+    fd(i * 2); rt(91)
+
+done()
